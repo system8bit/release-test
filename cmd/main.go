a0df93cbe0c47887ababe9d10c9ed484024f3aff
Suzuki Kentaro <suzuki.kentaro@moneyforward.co.jp>
2024-05-10 16:57:17 +0900

    add main. It returns itself version.

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	release_test "release-test"
+)
+
+func main() {
+	fmt.Printf("%s\n", release_test.Version)
+}
---
+++ version.go
@@ -0,0 +1,3 @@
+package release_test
+
+const Version = "v1.0.0"
